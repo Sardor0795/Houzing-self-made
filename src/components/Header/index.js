b7ebfd406d65aef2b2116644254fa6ea1f259ee7
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-02-25 14:50:23 +0500

    change href

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,31 +1,42 @@
 import React, { Component } from "react";
-import { HeaderContent, HeaderWratter, Li, LoginIconStyle, LogoIconStyle, Ul } from "./index.style";
-import {Container} from '../../global.styles'
-import LoginIcon from '../../assets/icons/login.svg'
-import LogoIcon from '../../assets/icons/logo.svg'
+import {
+  HeaderContent,
+  HeaderWratter,
+  Li,
+  LoginIconStyle,
+  LogoIconStyle,
+  Ul,
+} from "./index.style";
+import { Container } from "../../global.styles";
+import LoginIcon from "../../assets/icons/login.svg";
+import LogoIcon from "../../assets/icons/logo.svg";
 
 export class Header extends Component {
   render() {
-    return <HeaderWratter>
-
-    <Container>
-        <HeaderContent>
-            <LogoIconStyle href="http:#"><img src={LogoIcon} alt="logo" /></LogoIconStyle>
+    return (
+      <HeaderWratter>
+        <Container>
+          <HeaderContent>
+            <LogoIconStyle href="h#">
+              <img src={LogoIcon} alt="logo" />
+            </LogoIconStyle>
             <Ul>
-                <Li>
-                    <a href="http:#">Home</a>
-                </Li>
-                <Li>
-                    <a href="http:#">Properties</a>
-                </Li>
-                <Li last>
-                    <a href="http:#">Contacts</a>
-                </Li>
+              <Li>
+                <a href="h#">Home</a>
+              </Li>
+              <Li>
+                <a href="h#">Properties</a>
+              </Li>
+              <Li last>
+                <a href="h#">Contacts</a>
+              </Li>
             </Ul>
-            <button><LoginIconStyle src={LoginIcon} alt="img" /></button>
-        </HeaderContent>
-    </Container>
-
-    </HeaderWratter>;
+            <button>
+              <LoginIconStyle src={LoginIcon} alt="img" />
+            </button>
+          </HeaderContent>
+        </Container>
+      </HeaderWratter>
+    );
   }
 }
