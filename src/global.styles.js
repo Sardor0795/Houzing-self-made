d1edeef30cd436a02e3c5d9558afdb4060f0b9fe
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-02-25 13:23:19 +0500

    Prepare file structure

---
+++ src/global.styles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+    *{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        font-family: 'Montserrat', 'sans-serif';
+    }
+
+`;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { GlobalStyles } from "./global.styles";
+import { Container } from "./index.styled";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-
+    <GlobalStyles />
+    <Container>
+      <h1>header</h1>
+    </Container>
   </React.StrictMode>
 );
