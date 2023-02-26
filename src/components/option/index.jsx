ba1fdd45ee8a5c0895a4288d41dd66931256d735
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-02-26 22:04:35 +0500

    Make advanced btn

--- src/components/option/index.jsx
+++ src/components/option/index.jsx
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import { Container } from "../../global.style";
 import {
   AdvancedBtn,
+  AdvancedMenu,
+  ButtonsWrapper,
+  FormButton,
+  Input,
+  InputsGroup,
+  InputsWrapper,
+  InputTitle,
   OptionMenu,
   OptionWrapper,
   SearchAddresInput,
@@ -9,14 +16,51 @@ import {
 } from "./style";
 
 export class Option extends Component {
+  constructor() {
+    super();
+    this.state = {
+      advancedShow: "none",
+    };
+  }
   render() {
+    const openAdvanced = () => {
+      this.setState({
+        advancedShow: this.state.advancedShow === "none" ? "block" : "none",
+      });
+    };
     return (
       <OptionWrapper>
         <Container>
           <OptionMenu>
             <SearchAddresInput placeholder="Enter an address, neighborhood, city, or ZIP code" />
-            <AdvancedBtn>Advanced</AdvancedBtn>
+            <AdvancedBtn onClick={openAdvanced}>Advanced</AdvancedBtn>
             <SearchBtn searchBtn>Search</SearchBtn>
+            <AdvancedMenu display={this.state.advancedShow}>
+              <InputsWrapper>
+                <InputTitle>Address</InputTitle>
+                <InputsGroup>
+                  <Input placeholder="Country" />
+                  <Input placeholder="Region" />
+                  <Input placeholder="City" />
+                  <Input placeholder="Zip code" />
+                </InputsGroup>
+                <InputTitle>Apartment info</InputTitle>
+                <InputsGroup>
+                  <Input placeholder="Rooms" />
+                  <Input placeholder="Size" />
+                  <Input placeholder="Sort" />
+                </InputsGroup>
+                <InputTitle>Price</InputTitle>
+                <InputsGroup>
+                  <Input placeholder="Min price" />
+                  <Input placeholder="Max price" />
+                </InputsGroup>
+              </InputsWrapper>
+              <ButtonsWrapper>
+                <FormButton type="reset">Submit</FormButton>
+                <FormButton type="submit">Cancel</FormButton>
+              </ButtonsWrapper>
+            </AdvancedMenu>
           </OptionMenu>
         </Container>
       </OptionWrapper>
--- src/components/option/style.jsx
+++ src/components/option/style.jsx
@@ -25,6 +25,7 @@ export const OptionWrapper = styled.div`
 
 export const OptionMenu = styled.div`
   display: flex;
+  position: relative;
   @media (max-width: 760px) {
     flex-wrap: wrap;
   }
@@ -80,4 +81,69 @@ export const SearchBtn = styled.button`
     padding-left: 60px;
     background-position: left 35px center;
   }
+`;
+
+// Advanced menu
+
+export const AdvancedMenu = styled.form`
+  max-width: 920px;
+  width: 920px;
+  position: absolute;
+  top: 140%;
+  right: 0;
+  background-color: #ffffff;
+  border-radius: 5px;
+  display: ${({ display }) => display};
+`;
+
+export const InputsWrapper = styled.div`
+  padding: 30px;
+  padding-bottom: 0;
+`;
+
+export const InputTitle = styled.p`
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 24px;
+  color: #0d263b;
+  margin-bottom: 10px;
+`;
+
+export const InputsGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+`;
+
+export const Input = styled.input`
+  border: 1px solid #e6e9ec;
+  padding: 12px 16px;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  color: #0d263b;
+  width: 25%;
+  ::placeholder {
+    color: #0d263b;
+  }
+  :not(:last-child) {
+    margin-right: 20px;
+  }
+`;
+
+export const ButtonsWrapper = styled.div`
+  background: #f6f8f9;
+  border-radius: 0px 0px 5px 5px;
+  padding: 30px;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const FormButton = styled.button`
+  padding: 12px 40px;
+  background-color: ${({ type }) =>
+    type === "submit" ? "#0061DF" : "transparent"};
+  color: ${({ type }) => (type === "submit" ? "#FFFFFF" : "#0D263B")};
+  border: 1px solid ${({ type }) => (type === "submit" ? "#0061DF" : "#0D263B")};
+  border-radius: 2px;
+  margin-right: ${({ type }) => (type !== "submit" ? "20px" : "0")};
 `;
