5fea8085d8303df6721c2794e17a30d699ee39f8
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-02-28 13:01:58 +0500

    Change choose section to functional component

--- src/components/choose/index.jsx
+++ src/components/choose/index.jsx
@@ -1,4 +1,3 @@
-import React, { Component } from "react";
 import { Container } from "../../global.style";
 import {
   ChooseCard,
@@ -15,47 +14,43 @@ import ChooseHomeImg from "../../assets/icons/shoose_home.svg";
 import ChoosePhoneImg from "../../assets/icons/shoose_phone.svg";
 import ChooseMapImg from "../../assets/icons/shoose_map.svg";
 
-export class Choose extends Component {
-  render() {
-    return (
-      <ChooseWrapper>
-        <Container>
-          <ChooseTitle>Why Choose Us</ChooseTitle>
-          <ChooseText>
-            Nulla quis curabitur velit volutpat auctor bibendum consectetur sit.
-          </ChooseText>
-          <ChooseCards>
-            <ChooseCard mBottomSmall mBottom cardIcon={ChooseSmileImg}>
-              <ChooseCardTitle>Trusted By Thousands</ChooseCardTitle>
-              <ChooseCardText>
-                With over 1 million+ homes for sale available on the website,
-                Trulia can match you with a house you will want to call home.
-              </ChooseCardText>
-            </ChooseCard>
-            <ChooseCard mBottomSmall mBottom cardIcon={ChooseHomeImg} large>
-              <ChooseCardTitle>Wide Renge Of Properties</ChooseCardTitle>
-              <ChooseCardText>
-                With over 1 million+ homes for sale available on the website,
-                Trulia can match you with a house you will want to call home.
-              </ChooseCardText>
-            </ChooseCard>
-            <ChooseCard mBottomSmall cardIcon={ChoosePhoneImg}>
-              <ChooseCardTitle>Financing Made Easy</ChooseCardTitle>
-              <ChooseCardText>
-                With over 1 million+ homes for sale available on the website,
-                Trulia can match you with a house you will want to call home.
-              </ChooseCardText>
-            </ChooseCard>
-            <ChooseCard cardIcon={ChooseMapImg}>
-              <ChooseCardTitle>See Neighborhoods</ChooseCardTitle>
-              <ChooseCardText>
-                With over 1 million+ homes for sale available on the website,
-                Trulia can match you with a house you will want to call home.
-              </ChooseCardText>
-            </ChooseCard>
-          </ChooseCards>
-        </Container>
-      </ChooseWrapper>
-    );
-  }
-}
+export const Choose = () => (
+  <ChooseWrapper>
+    <Container>
+      <ChooseTitle>Why Choose Us</ChooseTitle>
+      <ChooseText>
+        Nulla quis curabitur velit volutpat auctor bibendum consectetur sit.
+      </ChooseText>
+      <ChooseCards>
+        <ChooseCard mBottomSmall mBottom cardIcon={ChooseSmileImg}>
+          <ChooseCardTitle>Trusted By Thousands</ChooseCardTitle>
+          <ChooseCardText>
+            With over 1 million+ homes for sale available on the website, Trulia
+            can match you with a house you will want to call home.
+          </ChooseCardText>
+        </ChooseCard>
+        <ChooseCard mBottomSmall mBottom cardIcon={ChooseHomeImg} large>
+          <ChooseCardTitle>Wide Renge Of Properties</ChooseCardTitle>
+          <ChooseCardText>
+            With over 1 million+ homes for sale available on the website, Trulia
+            can match you with a house you will want to call home.
+          </ChooseCardText>
+        </ChooseCard>
+        <ChooseCard mBottomSmall cardIcon={ChoosePhoneImg}>
+          <ChooseCardTitle>Financing Made Easy</ChooseCardTitle>
+          <ChooseCardText>
+            With over 1 million+ homes for sale available on the website, Trulia
+            can match you with a house you will want to call home.
+          </ChooseCardText>
+        </ChooseCard>
+        <ChooseCard cardIcon={ChooseMapImg}>
+          <ChooseCardTitle>See Neighborhoods</ChooseCardTitle>
+          <ChooseCardText>
+            With over 1 million+ homes for sale available on the website, Trulia
+            can match you with a house you will want to call home.
+          </ChooseCardText>
+        </ChooseCard>
+      </ChooseCards>
+    </Container>
+  </ChooseWrapper>
+);
