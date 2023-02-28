0ea3b466ff077276ff109047a56122261b0366a6
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-02-28 13:22:36 +0500

    Change footer_bottom section to functional comp

--- src/components/footer/footer__bottom/index.jsx
+++ src/components/footer/footer__bottom/index.jsx
@@ -1,23 +1,18 @@
-import React, { Component } from "react";
 import { Container } from "../../../global.style";
 import { Copyright, FooterBottomWrapper } from "./style";
 
 import LogoIcon from "../../../assets/icons/logo.svg";
 import { LogoIconStyle } from "../../header/style";
 
-export class FooterBottom extends Component {
-  render() {
-    return (
-      <Container>
-        <FooterBottomWrapper>
-          <LogoIconStyle bottom href="!#">
-            <img src={LogoIcon} alt="logo" />
-          </LogoIconStyle>
-          <Copyright>
-            Copyright © 2021 CreativeLayers. All Right Reserved.
-          </Copyright>
-        </FooterBottomWrapper>
-      </Container>
-    );
-  }
-}
+export const FooterBottom = () => (
+  <Container>
+    <FooterBottomWrapper>
+      <LogoIconStyle bottom href="!#">
+        <img src={LogoIcon} alt="logo" />
+      </LogoIconStyle>
+      <Copyright>
+        Copyright © 2021 CreativeLayers. All Right Reserved.
+      </Copyright>
+    </FooterBottomWrapper>
+  </Container>
+);
