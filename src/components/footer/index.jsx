7560a7fd3a4e24db9cc579ce110c2c4d49dc68f8
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-02-28 13:26:07 +0500

    Change footer to functional component

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,16 +1,11 @@
-import React, { Component } from "react";
 import { FooterBottom } from "./footer__bottom";
 import { FooterTop } from "./footer__top";
 import { FooterLine, FooterWrapper } from "./style";
 
-export class Footer extends Component {
-  render() {
-    return (
-      <FooterWrapper>
-        <FooterTop />
-        <FooterLine/>
-        <FooterBottom/>
-      </FooterWrapper>
-    );
-  }
-}
+export const Footer = () => (
+  <FooterWrapper>
+    <FooterTop />
+    <FooterLine />
+    <FooterBottom />
+  </FooterWrapper>
+);
