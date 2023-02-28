7eff5188b7c65f5e16d9421e8a5a1864d7c72c61
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-02-28 13:40:03 +0500

    Change header to functional component

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,3 @@
-import React, { Component } from "react";
 import {
   BurgerButton,
   HeaderContent,
@@ -13,52 +12,45 @@ import { Container } from "../../global.style";
 import LoginIcon from "../../assets/icons/login.svg";
 import LogoIcon from "../../assets/icons/logo.svg";
 import BurgerButtonIcon from "../../assets/icons/header_burger.svg";
-import {MobileMenu} from "./mobile_menu";
+import { MobileMenu } from "./mobile_menu";
+import { useState } from "react";
 
-export class Header extends Component {
-  constructor() {
-    super();
-    this.state = {
-      openMobileMenu: null,
-    };
-  }
-  render() {
-    const openMobileMenu = () => {
-      this.setState({
-        openMobileMenu: this.state.openMobileMenu === "open" ? null : "open",
-      });
-    };
-    return (
-      <HeaderWrapper className="header">
-        <Container>
-          <HeaderContent>
-            <BurgerButton
-              onClick={openMobileMenu}
-              icon={BurgerButtonIcon}
-              bgImg={this.state.openMobileMenu}
-            ></BurgerButton>
-            <LogoIconStyle href="!#">
-              <img src={LogoIcon} alt="logo" />
-            </LogoIconStyle>
-            <Ul>
-              <Li>
-                <a href="!#">Home</a>
-              </Li>
-              <Li>
-                <a href="!#">Properties</a>
-              </Li>
-              <Li last>
-                <a href="!#">Contacts</a>
-              </Li>
-            </Ul>
-            <LoginButtonStyle>
-              <LoginIconStyle src={LoginIcon} alt="img" />
-              Login
-            </LoginButtonStyle>
-          </HeaderContent>
-        </Container>
-        <MobileMenu menu={this.state.openMobileMenu} />
-      </HeaderWrapper>
-    );
-  }
-}
+export const Header = () => {
+  const [openMobileMenu, setOpenMobileMenu] = useState(null);
+
+  const openerMobileMenu = () => {
+    setOpenMobileMenu(openMobileMenu === "open" ? null : "open");
+  };
+  return (
+    <HeaderWrapper className="header">
+      <Container>
+        <HeaderContent>
+          <BurgerButton
+            onClick={openerMobileMenu}
+            icon={BurgerButtonIcon}
+            bgImg={openMobileMenu}
+          ></BurgerButton>
+          <LogoIconStyle href="!#">
+            <img src={LogoIcon} alt="logo" />
+          </LogoIconStyle>
+          <Ul>
+            <Li>
+              <a href="!#">Home</a>
+            </Li>
+            <Li>
+              <a href="!#">Properties</a>
+            </Li>
+            <Li last>
+              <a href="!#">Contacts</a>
+            </Li>
+          </Ul>
+          <LoginButtonStyle>
+            <LoginIconStyle src={LoginIcon} alt="img" />
+            Login
+          </LoginButtonStyle>
+        </HeaderContent>
+      </Container>
+      <MobileMenu menu={openMobileMenu} />
+    </HeaderWrapper>
+  );
+};
