2bea1c6cf3386c7ccff8532c98ad9bd207234e8a
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-02-28 16:10:07 +0500

    Add hero content

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,3 +1,19 @@
-import { HeroWrapper } from "./style";
+import { HeroButton, HeroMiniCard, HeroPrice, HeroText, HeroTitle, HeroWrapper } from "./style";
 
-export const Hero = () => <HeroWrapper></HeroWrapper>;
+import BedIcon from '../../assets/icons/hero_bed.svg'
+import BathIcon from '../../assets/icons/hero_bath.svg'
+import CarIcon from '../../assets/icons/hero_car.svg'
+import RulerIcon from '../../assets/icons/hero_ruler.svg'
+
+export const Hero = () => (
+  <HeroWrapper>
+    <HeroTitle>Skyper Pool Partment</HeroTitle>
+    <HeroText>112 Glenwood Ave Hyde Park, Boston, MA</HeroText>
+    <HeroMiniCard bgIcon={BedIcon}>4 beds</HeroMiniCard>
+    <HeroMiniCard bgIcon={BathIcon}>5 Baths</HeroMiniCard>
+    <HeroMiniCard bgIcon={CarIcon}>1 Garage</HeroMiniCard>
+    <HeroMiniCard bgIcon={RulerIcon} last>1200 Sq Ft</HeroMiniCard>
+    <HeroPrice>$5,250/mo</HeroPrice>
+    <HeroButton>Read more</HeroButton>
+  </HeroWrapper>
+);
--- src/components/hero/style.jsx
+++ src/components/hero/style.jsx
@@ -6,5 +6,56 @@ export const HeroWrapper = styled.div`
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center center;
+  padding: 134px 20px;
   min-height: 571px;
+  text-align: center;
+`;
+
+export const HeroTitle = styled.h1`
+  font-weight: 700;
+  font-size: 44px;
+  line-height: 48px;
+  letter-spacing: -0.02em;
+  color: #ffffff;
+  margin-bottom: 8px;
+`;
+
+export const HeroText = styled.p`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  color: #ffffff;
+  margin-bottom: 24px;
+`;
+
+export const HeroMiniCard = styled.span`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  color: #ffffff;
+  padding-top: 24px;
+  margin-right: ${({ last }) => (last ? "0px" : "24px")};
+  background-image: url("${({ bgIcon }) => bgIcon}");
+  background-repeat: no-repeat;
+  background-position: center top;
+  margin-bottom: 24px;
+`;
+
+export const HeroPrice = styled.div`
+  font-weight: 600;
+  font-size: 28px;
+  line-height: 36px;
+  letter-spacing: -0.02em;
+  color: #ffffff;
+  margin-bottom: 48px;
+`;
+
+export const HeroButton = styled.button`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  color: #ffffff;
+  padding: 12px 50px;
+  border: 1px solid #ffffff;
+  border-radius: 2px;
 `;
