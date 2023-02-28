ceb98fc9402a52ed7e6de03164bf88068caadddd
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-02-28 13:49:54 +0500

    Change option section to functional component

--- src/components/option/index.jsx
+++ src/components/option/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import { Container } from "../../global.style";
 import {
   AdvancedBtn,
@@ -15,69 +15,61 @@ import {
   SearchBtn,
 } from "./style";
 
-export class Option extends Component {
-  constructor() {
-    super();
-    this.state = {
-      advancedBtn: {
-        opacity: "0",
-        pointer_events: "none",
-      },
-    };
-  }
-  render() {
-    const openAdvanced = () => {
-      this.setState({
-        advancedBtn: {
-          opacity: this.state.advancedBtn.opacity === "0" ? "1" : "0",
-          pointer_events:
-            this.state.advancedBtn.pointer_events === "nona" ? "all" : "none",
-        },
-      });
-    };
-    return (
-      <OptionWrapper>
-        <Container>
-          <OptionMenu>
-            <SearchAddresInput placeholder="Enter an address, neighborhood, city, or ZIP code" />
-            <AdvancedBtn onClick={openAdvanced}>Advanced</AdvancedBtn>
-            <SearchBtn searchBtn>Search</SearchBtn>
-            <AdvancedMenu
-              pointerEvents={this.state.advancedBtn.pointer_events}
-              opacity={this.state.advancedBtn.opacity}
-            >
-              <InputsWrapper>
-                <InputTitle>Address</InputTitle>
-                <InputsGroup>
-                  <Input placeholder="Country" />
-                  <Input placeholder="Region" />
-                  <Input placeholder="City" />
-                  <Input placeholder="Zip code" />
-                </InputsGroup>
-                <InputTitle>Apartment info</InputTitle>
-                <InputsGroup>
-                  <Input placeholder="Rooms" />
-                  <Input placeholder="Size" />
-                  <Input placeholder="Sort" />
-                </InputsGroup>
-                <InputTitle>Price</InputTitle>
-                <InputsGroup>
-                  <Input placeholder="Min price" />
-                  <Input placeholder="Max price" />
-                </InputsGroup>
-              </InputsWrapper>
-              <ButtonsWrapper>
-                <FormButton onClick={openAdvanced} type="reset">
-                  Submit
-                </FormButton>
-                <FormButton onClick={openAdvanced} type="submit">
-                  Cancel
-                </FormButton>
-              </ButtonsWrapper>
-            </AdvancedMenu>
-          </OptionMenu>
-        </Container>
-      </OptionWrapper>
-    );
-  }
-}
+export const Option = () => {
+  const [advancedBtn, setAdvancedBtn] = useState({
+    opacity: "0",
+    pointer_events: "none",
+  });
+
+  const openAdvanced = () => {
+    setAdvancedBtn({
+      opacity: advancedBtn.opacity === "0" ? "1" : "0",
+      pointer_events: advancedBtn.pointer_events === "nona" ? "all" : "none",
+    });
+  };
+
+  return (
+    <OptionWrapper>
+      <Container>
+        <OptionMenu>
+          <SearchAddresInput placeholder="Enter an address, neighborhood, city, or ZIP code" />
+          <AdvancedBtn onClick={openAdvanced}>Advanced</AdvancedBtn>
+          <SearchBtn searchBtn>Search</SearchBtn>
+          <AdvancedMenu
+            pointerEvents={advancedBtn.pointer_events}
+            opacity={advancedBtn.opacity}
+          >
+            <InputsWrapper>
+              <InputTitle>Address</InputTitle>
+              <InputsGroup>
+                <Input placeholder="Country" />
+                <Input placeholder="Region" />
+                <Input placeholder="City" />
+                <Input placeholder="Zip code" />
+              </InputsGroup>
+              <InputTitle>Apartment info</InputTitle>
+              <InputsGroup>
+                <Input placeholder="Rooms" />
+                <Input placeholder="Size" />
+                <Input placeholder="Sort" />
+              </InputsGroup>
+              <InputTitle>Price</InputTitle>
+              <InputsGroup>
+                <Input placeholder="Min price" />
+                <Input placeholder="Max price" />
+              </InputsGroup>
+            </InputsWrapper>
+            <ButtonsWrapper>
+              <FormButton onClick={openAdvanced} type="reset">
+                Submit
+              </FormButton>
+              <FormButton onClick={openAdvanced} type="submit">
+                Cancel
+              </FormButton>
+            </ButtonsWrapper>
+          </AdvancedMenu>
+        </OptionMenu>
+      </Container>
+    </OptionWrapper>
+  );
+};
