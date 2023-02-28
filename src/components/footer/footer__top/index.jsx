2c6c17f72184a0cd91b5cd0bf25cab533ae90659
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-02-28 13:24:35 +0500

    Change footer_top section to functional component

--- src/components/footer/footer__top/index.jsx
+++ src/components/footer/footer__top/index.jsx
@@ -1,4 +1,3 @@
-import React, { Component } from "react";
 import { Container } from "../../../global.style";
 import {
   Contact,
@@ -25,115 +24,111 @@ import TwitterIcon from "../../../assets/icons/twitter.svg";
 import InstagramIcon from "../../../assets/icons/instagram.svg";
 import LinkedinIcon from "../../../assets/icons/linkedin.svg";
 
-export class FooterTop extends Component {
-  render() {
-    return (
-      <Container>
-        <FooterTopWrapper>
-          <Contact order_0>
-            <FooterTitle>Contact Us</FooterTitle>
-            <Ul>
-              <li>
-                <Locations textLg href="!#">
-                  <LocationIconStyled src={LocationIcon} alt="img" />
-                  329 Queensberry Street, North Melbourne VIC 3051, Australia.
-                </Locations>
-              </li>
-              <li>
-                <Locations href="!#">
-                  <LocationIconStyled src={PhoneIcon} alt="img" />
-                  123 456 7890
-                </Locations>
-              </li>
-              <li>
-                <Locations href="!#">
-                  <LocationIconStyled src={EmailIcon} alt="img" />
-                  support@houzing.com
-                </Locations>
-              </li>
-            </Ul>
-            <SocialsList>
-              <SocialsItem>
-                <SocialsLink>
-                  <img src={FacebookIcon} alt="img" />
-                </SocialsLink>
-              </SocialsItem>
-              <SocialsItem>
-                <SocialsLink>
-                  <img src={TwitterIcon} alt="img" />
-                </SocialsLink>
-              </SocialsItem>
-              <SocialsItem>
-                <SocialsLink>
-                  <img src={InstagramIcon} alt="img" />
-                </SocialsLink>
-              </SocialsItem>
-              <SocialsItem last>
-                <SocialsLink>
-                  <img src={LinkedinIcon} alt="img" />
-                </SocialsLink>
-              </SocialsItem>
-            </SocialsList>
-          </Contact>
-          <ExtraLinksWrapper >
-            <FooterTitle>Discover</FooterTitle>
-            <ExtraLinksList>
-              <ExtraLinksItem>
-                <ExtraLinksLink>Chicago</ExtraLinksLink>
-              </ExtraLinksItem>
-              <ExtraLinksItem>
-                <ExtraLinksLink>Los Angeles</ExtraLinksLink>
-              </ExtraLinksItem>
-              <ExtraLinksItem>
-                <ExtraLinksLink>Miami</ExtraLinksLink>
-              </ExtraLinksItem>
-              <ExtraLinksItem last>
-                <ExtraLinksLink>New York</ExtraLinksLink>
-              </ExtraLinksItem>
-            </ExtraLinksList>
-          </ExtraLinksWrapper>
-          <ExtraLinksWrapper order_1>
-            <FooterTitle>Lists by Category</FooterTitle>
-            <ExtraLinksList>
-              <ExtraLinksItem>
-                <ExtraLinksLink>Apartments</ExtraLinksLink>
-              </ExtraLinksItem>
-              <ExtraLinksItem>
-                <ExtraLinksLink>Condos</ExtraLinksLink>
-              </ExtraLinksItem>
-              <ExtraLinksItem>
-                <ExtraLinksLink>Houses</ExtraLinksLink>
-              </ExtraLinksItem>
-              <ExtraLinksItem>
-                <ExtraLinksLink>Offices</ExtraLinksLink>
-              </ExtraLinksItem>
-              <ExtraLinksItem>
-                <ExtraLinksLink>Retail</ExtraLinksLink>
-              </ExtraLinksItem>
-              <ExtraLinksItem last>
-                <ExtraLinksLink>Villas</ExtraLinksLink>
-              </ExtraLinksItem>
-            </ExtraLinksList>
-          </ExtraLinksWrapper>
-          <ExtraLinksWrapper>
-            <FooterTitle>Lists by Category</FooterTitle>
-            <ExtraLinksList>
-              <ExtraLinksItem>
-                <ExtraLinksLink>About Us</ExtraLinksLink>
-              </ExtraLinksItem>
-              <ExtraLinksItem>
-                <ExtraLinksLink>Terms & Conditions</ExtraLinksLink>
-              </ExtraLinksItem>
-              <ExtraLinksItem>
-                <ExtraLinksLink>Support Center</ExtraLinksLink>
-              </ExtraLinksItem>
-              <ExtraLinksItem last>
-                <ExtraLinksLink>Contact Us</ExtraLinksLink>
-              </ExtraLinksItem>
-            </ExtraLinksList>
-          </ExtraLinksWrapper>
-        </FooterTopWrapper>
-      </Container>
-    );
-  }
-}
+export const FooterTop = () => (
+  <Container>
+    <FooterTopWrapper>
+      <Contact order_0>
+        <FooterTitle>Contact Us</FooterTitle>
+        <Ul>
+          <li>
+            <Locations textLg href="!#">
+              <LocationIconStyled src={LocationIcon} alt="img" />
+              329 Queensberry Street, North Melbourne VIC 3051, Australia.
+            </Locations>
+          </li>
+          <li>
+            <Locations href="!#">
+              <LocationIconStyled src={PhoneIcon} alt="img" />
+              123 456 7890
+            </Locations>
+          </li>
+          <li>
+            <Locations href="!#">
+              <LocationIconStyled src={EmailIcon} alt="img" />
+              support@houzing.com
+            </Locations>
+          </li>
+        </Ul>
+        <SocialsList>
+          <SocialsItem>
+            <SocialsLink>
+              <img src={FacebookIcon} alt="img" />
+            </SocialsLink>
+          </SocialsItem>
+          <SocialsItem>
+            <SocialsLink>
+              <img src={TwitterIcon} alt="img" />
+            </SocialsLink>
+          </SocialsItem>
+          <SocialsItem>
+            <SocialsLink>
+              <img src={InstagramIcon} alt="img" />
+            </SocialsLink>
+          </SocialsItem>
+          <SocialsItem last>
+            <SocialsLink>
+              <img src={LinkedinIcon} alt="img" />
+            </SocialsLink>
+          </SocialsItem>
+        </SocialsList>
+      </Contact>
+      <ExtraLinksWrapper>
+        <FooterTitle>Discover</FooterTitle>
+        <ExtraLinksList>
+          <ExtraLinksItem>
+            <ExtraLinksLink>Chicago</ExtraLinksLink>
+          </ExtraLinksItem>
+          <ExtraLinksItem>
+            <ExtraLinksLink>Los Angeles</ExtraLinksLink>
+          </ExtraLinksItem>
+          <ExtraLinksItem>
+            <ExtraLinksLink>Miami</ExtraLinksLink>
+          </ExtraLinksItem>
+          <ExtraLinksItem last>
+            <ExtraLinksLink>New York</ExtraLinksLink>
+          </ExtraLinksItem>
+        </ExtraLinksList>
+      </ExtraLinksWrapper>
+      <ExtraLinksWrapper order_1>
+        <FooterTitle>Lists by Category</FooterTitle>
+        <ExtraLinksList>
+          <ExtraLinksItem>
+            <ExtraLinksLink>Apartments</ExtraLinksLink>
+          </ExtraLinksItem>
+          <ExtraLinksItem>
+            <ExtraLinksLink>Condos</ExtraLinksLink>
+          </ExtraLinksItem>
+          <ExtraLinksItem>
+            <ExtraLinksLink>Houses</ExtraLinksLink>
+          </ExtraLinksItem>
+          <ExtraLinksItem>
+            <ExtraLinksLink>Offices</ExtraLinksLink>
+          </ExtraLinksItem>
+          <ExtraLinksItem>
+            <ExtraLinksLink>Retail</ExtraLinksLink>
+          </ExtraLinksItem>
+          <ExtraLinksItem last>
+            <ExtraLinksLink>Villas</ExtraLinksLink>
+          </ExtraLinksItem>
+        </ExtraLinksList>
+      </ExtraLinksWrapper>
+      <ExtraLinksWrapper>
+        <FooterTitle>Lists by Category</FooterTitle>
+        <ExtraLinksList>
+          <ExtraLinksItem>
+            <ExtraLinksLink>About Us</ExtraLinksLink>
+          </ExtraLinksItem>
+          <ExtraLinksItem>
+            <ExtraLinksLink>Terms & Conditions</ExtraLinksLink>
+          </ExtraLinksItem>
+          <ExtraLinksItem>
+            <ExtraLinksLink>Support Center</ExtraLinksLink>
+          </ExtraLinksItem>
+          <ExtraLinksItem last>
+            <ExtraLinksLink>Contact Us</ExtraLinksLink>
+          </ExtraLinksItem>
+        </ExtraLinksList>
+      </ExtraLinksWrapper>
+    </FooterTopWrapper>
+  </Container>
+);
