62b2a7dfd1ea8899f9c4865269e0366aa6a9d479
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-02-28 13:51:22 +0500

    Change popular section to functional component

--- src/components/popular/index.jsx
+++ src/components/popular/index.jsx
@@ -1,15 +1,10 @@
-import React, { Component } from "react";
 import { PopularButton, PopularTitle, PopularWrapper } from "./style";
 
-export class Popular extends Component {
-  render() {
-    return (
-      <PopularWrapper>
-        <PopularTitle>
-          Vermont Farmhouse With Antique Jail Is the Week's Most Popular Home
-        </PopularTitle>
-        <PopularButton>Read more</PopularButton>
-      </PopularWrapper>
-    );
-  }
-}
+export const Popular = () => (
+  <PopularWrapper>
+    <PopularTitle>
+      Vermont Farmhouse With Antique Jail Is the Week's Most Popular Home
+    </PopularTitle>
+    <PopularButton>Read more</PopularButton>
+  </PopularWrapper>
+);
